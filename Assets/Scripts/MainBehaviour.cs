ec320e27b8ef33e7ad4f1b4d01b213ded1e75f54
Mads-Henri Ungethüm <48584414+MayorWolf@users.noreply.github.com>
2023-09-30 11:55:01 +0200

    [Modified] Build Settings

--- Assets/Scripts/MainBehaviour.cs
+++ Assets/Scripts/MainBehaviour.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Android;
+using Unity.Notifications.Android;
 
 public class MainBehaviour : MonoBehaviour
 {
@@ -15,10 +17,14 @@ public class MainBehaviour : MonoBehaviour
 
     private float _consumedWater = 0f;
     private float _consumptionGoal = 2.2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         AddHydration(0); //Update UI without changeing values
+        _NotificationSetup();
+
     }
 
     // Update is called once per frame
@@ -31,10 +37,35 @@ public class MainBehaviour : MonoBehaviour
         _consumedWater += addedWater;
         _consumedString.text = "Today's Goal: " + _consumedWater + "/" + _consumptionGoal + "l"; //Update string
         _slider.value = (_consumedWater == 0) ? 0 : (_consumedWater / _consumptionGoal); //update slider without dividing by 0 (because that would break the universe or something...)
+        PlayerPrefs.SetFloat("ConsumedWater", _consumedWater);
     }
 
     public void AddCustomHydration()
     {
         AddHydration(float.Parse(_customAmmountInput.text));
+    }
+
+    private void _NotificationSetup()
+    {
+        var channel = new AndroidNotificationChannel()
+        {
+            Id = "channel_id",
+            Name = "Default Channel",
+            Importance = Importance.Default,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+        //Get Notification Permissions
+        if (!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"))
+        {
+            Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
+        }
+        var notification = new AndroidNotification();
+        notification.Title = "Your Title";
+        notification.Text = "Your Text";
+        notification.FireTime = System.DateTime.Now.AddSeconds(15);
+
+        AndroidNotificationCenter.SendNotification(notification, "channel_id");
     }
 }
